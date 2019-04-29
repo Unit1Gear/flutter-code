83a3ed862592db23ba65a42b5db48b5b5ec25e31
nachourpi <nachourpi@hotmail.com>
2019-04-29 17:01:37 -0300

    project pubspec.yaml details and first compile ran

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:flutter_blue_example/widgets.dart';
+import 'widgets.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 
 void main() {
